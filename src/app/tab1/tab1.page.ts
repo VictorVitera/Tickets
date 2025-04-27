604bff2e340e524b177aeeae5acd0bcdf89ab0cd
Arthur Guedes <arthurhenrique96120@gmail.com>
2025-04-27 19:07:42 -0300

    project done

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { QueueService } from 'src/app/services/queue.service';
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
+  templateUrl: './tab1.page.html',
+  styleUrls: ['./tab1.page.scss'],
   standalone: false,
 })
 export class Tab1Page {
+  constructor(private queueService: QueueService) {}
 
-  constructor() {}
-
+  emitSenha(type: 'SP' | 'SE' | 'SG') {
+    const senha = this.queueService.emitSenha(type);
+    alert(`Senha emitida: ${senha}`);
+  }
 }
